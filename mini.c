329f24193ff84934ad69310250976968b0ffedad
2L00 <you@example.com>
2025-04-19 17:09:34 +0100

    some changes

--- mini.c
+++ mini.c
@@ -6,37 +6,36 @@
 /*   By: abddahma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 08:30:12 by abddahma          #+#    #+#             */
-/*   Updated: 2025/04/19 16:40:38 by abddahma         ###   ########.fr       */
+/*   Updated: 2025/04/19 17:08:51 by abddahma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int ac, char **av, char **env)
+int main()
 {
 	char **prompt;
 	char **split;
-	char *read_l;
+	char *read_l = NULL;
 	int i = 1;
 
 	while (1)
 	{
-		split = ft_split(av[i], ' ');
 
 //		if (av[i] == NULL)
 //			return 0;
 		read_l = readline("minishell->$ ");
 		if (read_l == NULL || *read_l == '\0')
 			continue;
-//			return 0;
-		printf("%s\n", read_l);
-		i++;
+		if (read_l)
+			split = ft_split(read_l, ' ');
+		i = 0;
+		while (split[i])
+		{
+			printf("%s\n", split[i]);
+			i++;
+		}
 	}
 
-	i = 0;
-	while (split[i])
-	{
-		printf("%s\n", split[i]);
-		i++;
-	}
+
 }
