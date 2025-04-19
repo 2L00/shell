1c503da80a2be06833dbe1b10896496cff38c919
2L00 <you@example.com>
2025-04-19 15:43:06 +0100

    hello

--- mini.c
+++ mini.c
@@ -6,26 +6,26 @@
 /*   By: abddahma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 08:30:12 by abddahma          #+#    #+#             */
-/*   Updated: 2025/04/19 14:12:19 by abddahma         ###   ########.fr       */
+/*   Updated: 2025/04/19 15:42:40 by abddahma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main()
+int main(int ac, char **av, char **env)
 {
 	char **av;
 	char **split;
 	char *read_l;
-//	if (ac < 2)
-//		return 0;
+	if (ac < 2)
+		return 0;
 	int i = 1;
 	while (1)
 	{
-		//split = ft_split(av[i], ' ');
+		split = ft_split(av[i], ' ');
 
-	//	if (av[i] == NULL)
-//			return 0;
+		if (av[i] == NULL)
+			return 0;
 		read_l = readline(av[i]);
 		if (read_l == NULL)
 			return 0;
@@ -34,9 +34,9 @@ int main()
 	}
 
 	i = 0;
-//	while (split[i])
-//	{
-//		printf("%s\n", split[i]);
-//		i++;
-//	}
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		i++;
+	}
 }
